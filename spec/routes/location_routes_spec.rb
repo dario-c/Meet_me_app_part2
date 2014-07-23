be587a342cbf32ccfc55e5c1dd75d4049b2fc83e
dario-c <dariocavanillas@gmail.com>
2014-07-23 10:43:26 +0200

    Minor text change

--- spec/routes/location_routes_spec.rb
+++ spec/routes/location_routes_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Locations Routing", :type => :routing do
 
-	it "routes to locations index" do
+	it "Routes to locations index" do
     expect(get: "/locations").to route_to("locations#index")
   end
 
-  it "routes to location with id" do
+  it "Routes to location with id" do
     expect(get: "/locations/999").to route_to(controller: "locations", action: "show", id: "999")
   end
 
