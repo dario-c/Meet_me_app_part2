16f83fc362d727b94e28d0d212cc51df2028f78b
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2014-07-23 10:41:09 +0200

    Added visit model validations

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,6 +1,11 @@
 class Visit < ActiveRecord::Base
 	belongs_to :location
 
+	validates :from_date, presence: true
+	validates :to_date, presence: true
+	validates :user_name, presence: true, format: {with: /\A[a-zA-Z]+\z/} #alphanumeric regex
+	validates :location_id, presence: true, numericality: true
+
 	def self.month_visits(month, year, value)
 		location = Location.find(value)
 		location.visits.where('extract(month from from_date) = ?', month).where('extract(year from from_date) = ?', year)
--- spec/models/visit_spec.rb
+++ spec/models/visit_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Visit, :type => :model do
 
+	it "from_date, to_date and user_name is present" do
+		item = Visit.new
+		expect(item.valid?).to eq(false)
+	end
+
+	it "user_name should only be alphanumeric characters" do
+		item = Visit.create user_name: "DarioJane-*&^%$", from_date: Date.today, to_date: Date.today+3
+		expect(item.valid?).to eq(false)
+	end
+
+	it "visit should be associated with a location" do
+		item = Visit.create location_id: nil, user_name: "Jane", from_date: Date.today, to_date: Date.today+3
+		expect(item.valid?).to eq(false)
+	end
+
 end
