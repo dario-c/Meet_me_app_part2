da3994c84a5c2d0eb9610069ea91fd3cbb251c82
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2014-07-23 09:33:14 +0200

    Added routes test file

---
+++ spec/routes/location_routes_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Locations Routing", :type => :routing do
+
+	it "routes to locations index" do
+    expect(get: "/locations").to route_to("locations#index")
+  end
+
+  it "routes to location with id" do
+    expect(get: "/locations/999").to route_to(controller: "locations", action: "show", id: "999")
+  end
+
+end
